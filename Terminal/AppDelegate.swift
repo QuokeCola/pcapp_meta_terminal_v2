5230165f6705ddeac5f46dc5d84c98de521de7c2
QuokeCola <quokecola@icloud.com>
2020-01-28 16:35:36 +0800

    Initial Commit

---
+++ Terminal/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Terminal
+//
+//  Created by 钱晨 on 2020/1/28.
+//  Copyright © 2020年 钱晨. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
