68425e49a2afbe3534aa303094629fba87af3e03
QuokeCola <quokecola@icloud.com>
2020-01-28 18:17:51 +0800

    Added ORSSerialPort submodule

---
+++ Terminal/MainController.swift
@@ -0,0 +1,18 @@
+//
+//  MainController.swift
+//  Terminal
+//
+//  Created by 钱晨 on 2020/1/28.
+//  Copyright © 2020年 钱晨. All rights reserved.
+//
+
+import Cocoa
+
+class MainController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
