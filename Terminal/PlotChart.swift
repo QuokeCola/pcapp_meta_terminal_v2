2dc988722738a93255d2533839f26c3e7a372c00
QuokeCola <quokecola@icloud.com>
2020-01-30 23:45:09 +0800

    Revised a Error in Chart Plot. We need to plot the data from the rear of the queue

--- Terminal/PlotChart.swift
+++ Terminal/PlotChart.swift
@@ -28,27 +28,30 @@ class PlotChart: SKScene {
         // Set start point
         let realdata = real_data.getData()
         let targetdata = target_data.getData()
-            
+        
+        var ABS_Datarange = self.dataRange.MaxValue - self.dataRange.MinValue
+        
         // Plot data, add time scale later
         if(realdata.count != 0){
             // Get data
             
-            let latest_data = realdata[0]
-            var index = 0
-            realdatapath.move(to: CGPoint(x: 0.0, y: 0.0))
-            targetdatapath.move(to: CGPoint(x: 0.0, y: 0.0))
+            let latest_data = realdata[realdata.count-1]
+            var index = realdata.count - 1
+            realdatapath.move(to: CGPoint(x: self.size.width, y: 0.0))
+            targetdatapath.move(to: CGPoint(x: self.size.width, y: 0.0))
             
-            while (latest_data.Time - realdata[index].Time < time_reveal && index < realdata.count - 1) {
-                let pointx = CGFloat(Float((realdata[index].Time) - latest_data.Time)/Float(time_reveal)) * self.size.width
+            while (latest_data.Time - realdata[index].Time < time_reveal && index > 0) {
                 
-                let realdatapointy = CGFloat((realdata[index].Data)/Float(Max_Value)) * self.size.height
-                let targetdatapointy = CGFloat((targetdata[index].Data)/Float(Max_Value)) * self.size.height
+                let pointx = CGFloat(Float((realdata[index].Time) - latest_data.Time)/Float(time_reveal)) * self.size.width + self.size.width
+                print(pointx)
+                let realdatapointy = CGFloat((realdata[index].Data)/Float(ABS_Datarange)) * self.size.height
+                let targetdatapointy = CGFloat((targetdata[index].Data)/Float(ABS_Datarange)) * self.size.height
                 
                 let realdatapoint = CGPoint(x: pointx, y: realdatapointy)
                 let targetdatapoint = CGPoint(x: pointx, y: targetdatapointy)
                 realdatapath.addLine(to: realdatapoint)
                 targetdatapath.addLine(to: targetdatapoint)
-                index = index + 1
+                index = index - 1
             }
         }
         // Set line Style
@@ -64,8 +67,8 @@ class PlotChart: SKScene {
         addChild(realdatachartpath)
         addChild(targetdatachartpath)
         
-        realdatachartpath.position = CGPoint(x: 0, y: 0)
-        targetdatachartpath.position = CGPoint(x: 0, y: 0)
+        realdatachartpath.position = CGPoint(x: 0, y: self.size.height*0.5)
+        targetdatachartpath.position = CGPoint(x: 0, y: self.size.height*0.5)
         self.dataChanged = false
     }
     
@@ -90,8 +93,12 @@ class PlotChart: SKScene {
     /***--------------------Data Storage----------------------***/
     
     var time_reveal = 20000
-    var Max_Value = 200
     var dataChanged: Bool = false
+    struct dataRange_t {
+        var MaxValue: Float
+        var MinValue: Float
+    }
+    var dataRange = dataRange_t(MaxValue: 200.0, MinValue: -200.0)
     // A Basic Point
     struct Points {
         var Data: Float = 0.0
