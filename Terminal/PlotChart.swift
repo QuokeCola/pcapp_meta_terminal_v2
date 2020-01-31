ff952e53bc0b323f8571a20690a3222114b99ee9
QuokeCola <quokecola@icloud.com>
2020-01-31 19:51:54 +0800

    Add Axis and Label for plot chart

--- Terminal/PlotChart.swift
+++ Terminal/PlotChart.swift
@@ -30,8 +30,6 @@ class PlotChart: SKScene {
         let realdata = real_data.getData()
         let targetdata = target_data.getData()
         
-        let ABS_Datarange = self.dataRange.MaxValue - self.dataRange.MinValue
-        
         // Plot data, add time scale later
         if(realdata.count != 0){
             
@@ -42,12 +40,34 @@ class PlotChart: SKScene {
             var index = realdata.count - 1
             realdatapath.move(to: CGPoint(x: self.size.width, y: 0.0))
             targetdatapath.move(to: CGPoint(x: self.size.width, y: 0.0))
+            var maxData: Float = 30.0
+            var minData: Float = -30.0
             
             while (abs(latest_data.Time - realdata[index].Time) < time_reveal && index > 0) {
                 
+                // Auto Size
+                
+                //Get the maxdata and mindata in a loop
+                if(realdata[index].Data > maxData || targetdata[index].Data > maxData) {
+                    if(realdata[index].Data > targetdata[index].Data) {
+                        maxData = realdata[index].Data
+                    } else {
+                        maxData = targetdata[index].Data
+                    }
+                }
+
+                if(realdata[index].Data < minData || targetdata[index].Data < minData) {
+                    if(realdata[index].Data < targetdata[index].Data) {
+                        minData = realdata[index].Data
+                    } else {
+                        minData = realdata[index].Data
+                    }
+                }
+                
                 let pointx = CGFloat(Float((realdata[index].Time) - latest_data.Time)/Float(time_reveal)) * self.size.width + self.size.width
-                let realdatapointy = CGFloat((realdata[index].Data)/Float(ABS_Datarange)) * self.size.height
-                let targetdatapointy = CGFloat((targetdata[index].Data)/Float(ABS_Datarange)) * self.size.height
+                let realdatapointy = mapDataToPlot(data: realdata[index].Data)
+                let targetdatapointy = mapDataToPlot(data: targetdata[index].Data)
+//                let targetdatapointy = CGFloat((targetdata[index].Data)/Float(dataRange.ABSDataRange())) * self.size.height
                 
                 let realdatapoint = CGPoint(x: pointx, y: realdatapointy)
                 let targetdatapoint = CGPoint(x: pointx, y: targetdatapointy)
@@ -55,7 +75,57 @@ class PlotChart: SKScene {
                 targetdatapath.addLine(to: targetdatapoint)
                 index = index - 1
             }
+            // Update MaxValue and MinValue, while left some void.
+            self.dataRange.MaxValue = maxData+20.0
+            self.dataRange.MinValue = minData-20.0
         }
+        
+        // Plot the chart background
+        let basic_line_digit = String(Int(dataRange.ABSDataRange())).count
+        var basic_seperation: Double = 1
+        if(dataRange.ABSDataRange()/pow(10.0, Float(basic_line_digit-1)) > 2.0) {
+            basic_seperation = pow(Double(10), Double(basic_line_digit - 1))
+        } else {
+            basic_seperation = pow(Double(10), Double(basic_line_digit - 2))
+        }
+        let backgroundplot = CGMutablePath()
+        let lowerBound = Float(Int(dataRange.MinValue/Float(basic_seperation))) * Float(basic_seperation)
+        
+        backgroundplot.move(to: CGPoint(x: -1.0, y: mapDataToPlot(data: Float(lowerBound))))
+        for i in stride(from: 0,to: Int(dataRange.ABSDataRange()/Float(basic_seperation)),by: 2) {
+            
+            let currentHeight = lowerBound + Float(i) * Float(basic_seperation)
+            backgroundplot.addLine(to: CGPoint(x:self.size.width+1.0, y: mapDataToPlot(data: currentHeight)))
+            backgroundplot.addLine(to: CGPoint(x:self.size.width+1.0, y: mapDataToPlot(data: currentHeight+Float(basic_seperation))))
+            backgroundplot.addLine(to: CGPoint(x: -1.0, y: mapDataToPlot(data: currentHeight+Float(basic_seperation))))
+            backgroundplot.addLine(to: CGPoint(x: -1.0, y: mapDataToPlot(data: currentHeight+2*Float(basic_seperation))))
+            
+            let label_low = SKLabelNode(text: "\(currentHeight)")
+            let label_high = SKLabelNode(text: "\(currentHeight+Float(basic_seperation))")
+            
+            label_low.verticalAlignmentMode = .bottom;
+            label_low.horizontalAlignmentMode = .right
+            label_low.fontColor = SKColor.lightGray
+            label_low.fontName = "Din Condensed"
+            label_low.fontSize = 10
+            label_low.position = CGPoint(x: self.size.width, y: mapDataToPlot(data: currentHeight))
+            
+            label_high.verticalAlignmentMode = .bottom;
+            label_high.horizontalAlignmentMode = .right
+            label_high.fontColor = SKColor.lightGray
+            label_high.fontName = "Din Condensed"
+            label_high.fontSize = 10
+            let higherHeight = currentHeight + Float(basic_seperation)
+            label_high.position = CGPoint(x: self.size.width, y: mapDataToPlot(data: higherHeight))
+            
+            addChild(label_low)
+            addChild(label_high)
+        }
+        let backgroundpath = SKShapeNode(path: backgroundplot)
+        backgroundpath.lineWidth = 1
+        backgroundpath.strokeColor = NSColor.lightGray
+        backgroundpath.position = CGPoint(x: 0.0, y: 0.0)
+
         // Set line Style
         let realdatachartpath = SKShapeNode(path: realdatapath)
         realdatachartpath.lineWidth = 1
@@ -69,20 +139,28 @@ class PlotChart: SKScene {
         titlebar.verticalAlignmentMode = .bottom;
         titlebar.horizontalAlignmentMode = .left
         titlebar.fontColor = SKColor.black
-        titlebar.fontName = "System Regular"
+        titlebar.fontName = "Din Condensed"
         titlebar.fontSize = 10
         titlebar.position = CGPoint(x: 5.0, y: 3.0)
-        addChild(titlebar)
         
+        backgroundpath.alpha = 0.3
         // Show path
+        addChild(titlebar)
         addChild(realdatachartpath)
         addChild(targetdatachartpath)
+        addChild(backgroundpath)
 
-        realdatachartpath.position = CGPoint(x: 0, y: self.size.height*0.5)
-        targetdatachartpath.position = CGPoint(x: 0, y: self.size.height*0.5)
+        realdatachartpath.position = CGPoint(x: 0, y: 0)
+        targetdatachartpath.position = CGPoint(x: 0, y: 0)
 
         self.dataChanged = false
     }
+    /***------------------------Plot Methods-------------------------***/
+    
+    func mapDataToPlot(data: Float)->CGFloat{
+        return CGFloat((data - dataRange.MinValue)/Float(dataRange.ABSDataRange())) * self.size.height
+    }
+    
     
     /***----------------------Data Management------------------------***/
     func AddData(RealData_: Float, TargetData_: Float, Time_: Int) {
@@ -113,7 +191,11 @@ class PlotChart: SKScene {
     struct dataRange_t {
         var MaxValue: Float
         var MinValue: Float
+        func ABSDataRange() -> Float {
+            return MaxValue - MinValue
+        }
     }
+    
     var dataRange = dataRange_t(MaxValue: 200.0, MinValue: -200.0)
     // A Basic Point
     struct Points {
